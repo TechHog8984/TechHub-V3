424b594e56a8945161a3a419a923d7ceeb5b84d3
TechHog <77560013+TechHog8984@users.noreply.github.com>
2021-11-05 19:51:31 -0400

    Create gamelist.lua

---
+++ info/gamelist.lua
@@ -0,0 +1,10 @@
+{
+    "Arsenal" :  286090429,
+    "Sound Space" : 2677609345,
+    "Be a Parkour Ninja" : 147848991,
+    "Rage Runner" : 6073366803,
+    "Gods of Glory" : 5665787539, 
+    "Risky Rollers" : 6065752559,
+    "Roll Call" : 6251081051,
+    "Tower of Hell" : 1962086868,
+}
