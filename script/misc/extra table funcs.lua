dfa521a0c96e31b76a59df36544d8572c0f10639
TechHog <77560013+TechHog8984@users.noreply.github.com>
2021-11-12 17:33:44 -0500

    Create extra table funcs.lua

---
+++ script/misc/extra table funcs.lua
@@ -0,0 +1,16 @@
+local function tablefindlower(Table, Value)
+  for I, V in next, Table do
+    if tostring(V):lower() == tostring(Value):lower() then
+      return I, V
+    end
+  end
+end
+local function tablefindlowerByIndex(Table, Index)
+  for I, V in next, Table do
+    if tostring(I):lower() == tostring(Index):lower() then
+      return I, V
+    end
+  end
+end
+
+return {lower = tablefindlower, lowerByIndex = tablefindlowerByIndex}
