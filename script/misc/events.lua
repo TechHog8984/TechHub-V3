2dafa278c152dbdfa975b78dfe71d0e3fc8f3afa
TechHog <77560013+TechHog8984@users.noreply.github.com>
2021-11-14 20:01:10 -0500

    Create events.lua

---
+++ script/misc/events.lua
@@ -0,0 +1,52 @@
+local EventManager = {events = {}}
+
+function EventManager:GetEvents()
+	return self.events
+end
+function EventManager:GetEvent(id)
+	if self[id] then
+		return self[id]
+	end
+	return nil
+end
+
+function EventManager:CreateEvent(id)
+	local event = {}
+	local connections = {}
+
+	function event:Connect(func)
+		local connection = {func = func, enabled = true}
+
+		function connection:Disconnect()
+			connection.func = nil
+			connection.enabled = false
+			table.remove(table.find(connections, connection))
+			connection = nil
+		end
+
+		table.insert(connections, connection)
+		return connection
+	end
+
+	function event:DisconnectAll()
+		for i, connection in pairs(connections) do
+			connection:Disconnect()
+		end
+		connections = {}
+	end
+
+	function event:Fire(...)
+		local args = {...}
+		for i, connection in pairs(connections) do
+			if connection and connection.func and connection.enabled == true then
+				coroutine.wrap(function()connection.func(table.unpack(args))end)
+			end
+		end
+	end
+
+	local realid = id or #self
+	self[realid] = event
+	return event
+end
+
+return EventManager
