624e61df99099f58b0c77274bc31cc7262c1a1f1
TechHog <77560013+TechHog8984@users.noreply.github.com>
2021-11-14 20:41:46 -0500

    Update gamelist.lua

--- info/gamelist.lua
+++ info/gamelist.lua
@@ -1,10 +1,10 @@
 {
     "Arsenal" :  286090429,
-    "Sound Space" : 2677609345,
     "Be a Parkour Ninja" : 147848991,
-    "Rage Runner" : 6073366803,
     "Gods of Glory" : 5665787539, 
+    "Rage Runner" : 6073366803,
     "Risky Rollers" : 6065752559,
     "Roll Call" : 6251081051,
+    "Sound Space" : 2677609345,
     "Tower of Hell" : 1962086868
 }
